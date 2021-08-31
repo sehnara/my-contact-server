f735ddffb267cfea5a4addce15f148a51f260c25
sehoon <gsh723@naver.com>
2021-08-31 13:51:05 +0900

    no message

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ContactsModule } from './contacts/contacts.module';
-
 @Module({
   imports: [TypeOrmModule.forRoot(), ContactsModule],
   controllers: [AppController, ],
